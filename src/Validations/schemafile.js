87227393188ba2cd11ce25e7a8aa7e08ba5199c0
Sombhartigoswami <sombharatigoswami@gmail.com>
2026-06-12 15:02:32 +0530

    Update schemafile.js

--- src/Validations/schemafile.js
+++ src/Validations/schemafile.js
@@ -1,8 +1,21 @@
-import * as yup from "yup"
-export const schemafile = yup.object({
-    // validation rules
-    name:yup.string().required("Name is required"),
-    email:yup.string().email("Invalid email").required("Email is required"),
-    age:yup.number().positive().integer().required("Age is required"),
+// import * as yup from "yup"
+// export const schemafile = yup.object({
+//     // validation rules
+//     name:yup.string().required("Name is required"),
+//     email:yup.string().email("Invalid email").required("Email is required"),
+//     age:yup.number().positive().integer().required("Age is required"),
 
-})
+// })
+import * as yup from "yup";
+export const schemafile = yup.object({
+  // validation rules
+  name: yup.string().required("Name is required"),
+  phone: yup
+    .string()
+    .required("Phone Number is required")
+    .matches(/^[0-9]{10}$/, "Enter valid 10 digit number"),
+  date: yup.string().required("Register Date is required"),
+  fees: yup.string().required("Fees Status is required"),
+  seat: yup.string().required("Seat Availability is required"),
+  seatno: yup.string().required("Seat no is required"),
+});
