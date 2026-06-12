32c00fd96eb7e1a70b25c9134df06cf9519b7b7e
Sombhartigoswami <sombharatigoswami@gmail.com>
2026-06-12 15:08:02 +0530

    Update AddEdit.jsx

--- src/Pages/AddEdit.jsx
+++ src/Pages/AddEdit.jsx
@@ -1,3 +1,84 @@
+// import { useEffect } from "react";
+// import { useNavigate, useParams } from "react-router-dom";
+// import Api from "../Api/axios";
+
+// import { useForm, Controller } from "react-hook-form";
+// import { yupResolver } from "@hookform/resolvers/yup";
+// import { schemafile } from "../Validations/schemafile";
+
+// import { InputText } from "primereact/inputtext";
+// import { InputNumber } from "primereact/inputnumber";
+// import { Button } from "primereact/button";
+
+// function AddEdit() {
+//   const navigate = useNavigate();
+//   const { id } = useParams();
+//   // react hook form's all variables
+//   const {
+//     register,
+//     handleSubmit,
+//     control,
+//     setValue,
+//     formState: { errors },
+//   } = useForm({ resolver: yupResolver(schemafile) });
+
+//   //   data varible hold all form data
+//   const submitFun = async (data) => {
+//     if (id) {
+//       await Api.put(`/students/${id}`, data);
+//     } else {
+//       // add data in file
+//       await Api.post("/students", data);
+//     }
+//     navigate("/");
+//   };
+
+//   // useEffect hook for Edit Data
+//   useEffect(() => {
+//     if (id) {
+//       Api.get(`/students/${id}`).then((res) => {
+//         Object.keys(res.data).map((key) => {
+//           setValue(key, res.data[key]);
+//         });
+//       });
+//     }
+//   }, [id, setValue]);
+
+//   return (
+//     <div>
+//       <h3>{id ? "Edit Student Details" : "Add Student Details"}</h3>
+//       <form onSubmit={handleSubmit(submitFun)}>
+//         <div>
+//           <label>Name</label>
+//           <InputText {...register("name")} style={{ width: "100%" }} />
+//           <small>{errors.name?.message}</small>
+//         </div>
+//         <div>
+//           <label>Email</label>
+//           <InputText {...register("email")} style={{ width: "100%" }} />
+//           <small>{errors.email?.message}</small>
+//         </div>
+//         <div>
+//           <label>Age</label>
+//           <Controller
+//             name="age"
+//             control={control}
+//             render={({ field }) => (
+//               <InputNumber
+//                 value={field.value}
+//                 onValueChange={(e) => field.onChange(e.value)}
+//                 style={{ width: "100%" }}
+//               />
+//             )}
+//           />
+//           <small>{errors.age?.message}</small>
+//         </div>
+//         <Button label="submit" style={{ marginTop: "5px" }} />
+//       </form>
+//     </div>
+//   );
+// }
+// export default AddEdit;
 import { useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import Api from "../Api/axios";
@@ -22,15 +103,16 @@ function AddEdit() {
     formState: { errors },
   } = useForm({ resolver: yupResolver(schemafile) });
 
-  //   data varible hold all form data
+  // data varible hold all form data
   const submitFun = async (data) => {
+    // console.log(data);
     if (id) {
       await Api.put(`/students/${id}`, data);
     } else {
       // add data in file
       await Api.post("/students", data);
     }
-    navigate("/");
+    navigate("/users");
   };
 
   // useEffect hook for Edit Data
@@ -49,29 +131,38 @@ function AddEdit() {
       <h3>{id ? "Edit Student Details" : "Add Student Details"}</h3>
       <form onSubmit={handleSubmit(submitFun)}>
         <div>
+          <label>Register Date</label>
+          <InputText
+            type="date"
+            {...register("date")}
+            style={{ width: "100%" }}
+          />
+          <small>{errors.date?.message}</small>
+        </div>
+        <div>
           <label>Name</label>
           <InputText {...register("name")} style={{ width: "100%" }} />
           <small>{errors.name?.message}</small>
         </div>
         <div>
-          <label>Email</label>
-          <InputText {...register("email")} style={{ width: "100%" }} />
-          <small>{errors.email?.message}</small>
+          <label>PhoneNo</label>
+          <InputText {...register("phone")} style={{ width: "100%" }} />
+          <small>{errors.phone?.message}</small>
         </div>
         <div>
-          <label>Age</label>
-          <Controller
-            name="age"
-            control={control}
-            render={({ field }) => (
-              <InputNumber
-                value={field.value}
-                onValueChange={(e) => field.onChange(e.value)}
-                style={{ width: "100%" }}
-              />
-            )}
-          />
-          <small>{errors.age?.message}</small>
+          <label>fees status</label>
+          <InputText {...register("fees")} style={{ width: "100%" }} />
+          <small>{errors.fees?.message}</small>
+        </div>
+        <div>
+          <label>Seat Availability</label>
+          <InputText {...register("seat")} style={{ width: "100%" }} />
+          <small>{errors.seat?.message}</small>
+        </div>
+        <div>
+          <label>Seat No</label>
+          <InputText {...register("seatno")} style={{ width: "100%" }} />
+          <small>{errors.seatno?.message}</small>
         </div>
         <Button label="submit" style={{ marginTop: "5px" }} />
       </form>
@@ -79,3 +170,4 @@ function AddEdit() {
   );
 }
 export default AddEdit;
+
