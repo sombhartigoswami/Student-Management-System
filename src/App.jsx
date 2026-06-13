77b48f4e801f010d8bf8b577d5ce1391ae3476ae
Sombhartigoswami <sombharatigoswami@gmail.com>
2026-06-13 10:28:12 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,3 @@
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import UserList from "./Pages/UserList";
-// import AddEdit from "./Pages/AddEdit";
-
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       <Routes>
-//         <Route path="/" element={<UserList />} />
-//         <Route path="/add" element={<AddEdit />} />
-//         <Route path="/edit/:id" element={<AddEdit />} />
-//       </Routes>
-//     </BrowserRouter>
-//   );
-// }
-
-// export default App;
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UserList from "./Pages/UserList";
 import AddEdit from "./Pages/AddEdit";
@@ -32,6 +15,5 @@ function App() {
     </BrowserRouter>
   );
 }
-
 export default App;
 
