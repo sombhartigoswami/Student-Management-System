f2291d833ec84a86439a695b3d25f8244e9a47be
Sombhartigoswami <sombharatigoswami@gmail.com>
2026-06-13 10:27:01 +0530

    Update schemafile.js

--- src/Validations/schemafile.js
+++ src/Validations/schemafile.js
@@ -1,11 +1,3 @@
-// import * as yup from "yup"
-// export const schemafile = yup.object({
-//     // validation rules
-//     name:yup.string().required("Name is required"),
-//     email:yup.string().email("Invalid email").required("Email is required"),
-//     age:yup.number().positive().integer().required("Age is required"),
-
-// })
 import * as yup from "yup";
 export const schemafile = yup.object({
   // validation rules
