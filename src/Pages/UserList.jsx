b9367c9f2f0ca2e090c73626325ee33a91e3a966
Sombhartigoswami <sombharatigoswami@gmail.com>
2026-06-13 10:26:18 +0530

    Update UserList.jsx

--- src/Pages/UserList.jsx
+++ src/Pages/UserList.jsx
@@ -1,125 +1,3 @@
-// import { useEffect, useState } from "react";
-// import Api from "../Api/axios";
-// import { useNavigate } from "react-router-dom";
-
-// import { DataTable } from "primereact/datatable";
-// import { Column } from "primereact/column";
-// import { Button } from "primereact/button";
-
-// function UserList() {
-//   const [userdata, setUserdata] = useState([]);
-//   const [loading, setLoading] = useState(true);
-//   const navigate = useNavigate();
-
-//   const getUsers = async () => {
-//     setLoading(true);
-//     const response = await Api.get("/students");
-//     // console.log(response);
-//     setUserdata(response.data);
-//     setLoading(false);
-//   };
-
-//   useEffect(() => {
-//     getUsers();
-//   }, []);
-
-//   // function for Delete Student Data
-//   const terminateStudent = async (id) => {
-//     // console.log("delete button was clicked");
-//     await Api.delete(`/students/${id}`);
-//     getUsers();
-//   };
-
-//   // function for Edit and Delete Button
-//   const actionBtn = (rowdata) => (
-//     <>
-//       <Button
-//         label="Edit"
-//         onClick={() => navigate(`/edit/${rowdata.id}`)}
-//         style={{
-//           margin: "5px",
-//           background: "yellowgreen",
-//           padding: "7px",
-//           border: "none",
-//         }}
-//       />
-//       <Button
-//         label="Delete"
-//         onClick={() => terminateStudent(rowdata.id)}
-//         style={{
-//           margin: "5px",
-//           background: "red",
-//           padding: "7px",
-//           border: "none",
-//         }}
-//       />
-//     </>
-//   );
-//   // add loading 
-//   if(loading){
-//     return <h2>Loading Student Data Please Wait....</h2>;
-//   }
-//   return (
-//     <div>
-//       <div
-//         style={{
-//           display: "flex",
-//           justifyContent: "space-evenly",
-//           background: "lightgreen",
-//         }}
-//       >
-//         <h2>STUDENT MANAGEMENT SYSTEM</h2>
-//         <Button
-//           label="Add"
-//           style={{ margin: "10px" }}
-//           onClick={() => navigate("/add")}
-//         />
-//       </div>
-//       <DataTable
-//         value={userdata}
-//         showGridlines
-//         stripedRows
-//         paginator
-//         rows={4}
-//         size="small"
-//         filterDisplay="row"
-//         tableStyle={{ minWidth: "50rem" }}
-//       >
-//         <Column field="id" header="Id"></Column>
-//         <Column
-//           field="name"
-//           header="Name"
-//           filter
-//           filterPlaceholder="Search"
-//           style={{ width: "25%" }}
-//           sortable
-//         ></Column>
-//         <Column
-//           field="email"
-//           header="Email"
-//           filter
-//           filterPlaceholder="Search"
-//           style={{ width: "25%" }}
-//           sortable
-//         ></Column>
-//         <Column
-//           field="age"
-//           header="Age"
-//           filter
-//           filterPlaceholder="Search"
-//           style={{ width: "25%" }}
-//           sortable
-//         ></Column>
-//         <Column
-//           header="Action"
-//           body={actionBtn}
-//           style={{ width: "13%" }}
-//         ></Column>
-//       </DataTable>
-//     </div>
-//   );
-// }
-// export default UserList;
 import { useEffect, useState } from "react";
 import Api from "../Api/axios";
 import { useNavigate } from "react-router-dom";
@@ -132,7 +10,6 @@ function UserList() {
   const [userdata, setUserdata] = useState([]);
   const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
-
   const getUsers = async () => {
     setLoading(true);
     const response = await Api.get("/students");
@@ -140,7 +17,6 @@ function UserList() {
     setUserdata(response.data);
     setLoading(false);
   };
-
   useEffect(() => {
     getUsers();
   }, []);
@@ -210,7 +86,6 @@ function UserList() {
     margin: "5px"
   }}
 />
-
 <Button
   label="Add"
   onClick={() => navigate("/add")}
@@ -225,16 +100,6 @@ function UserList() {
     margin: "5px"
   }}
 />
-        {/* <Button
-          label="Logout"
-          onClick={handleLogout}
-          style={{ margin: "10px" }}
-        />
-        <Button
-          label="Add"
-          style={{ margin: "10px" }}
-          onClick={() => navigate("/add")}
-        /> */}
       </div>
       <DataTable
         value={userdata}
